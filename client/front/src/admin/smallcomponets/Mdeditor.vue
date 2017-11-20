b23f33eb714960702c55a2ebc11dac209c1adcdf
caixiaocong <caistrong@outlook.com>
2017-11-20 17:16:52 +0800

    a more step for markdown-editor

--- client/front/src/admin/smallcomponets/Mdeditor.vue
+++ client/front/src/admin/smallcomponets/Mdeditor.vue
@@ -7,17 +7,41 @@
 <script>
 import SimpleMDE from "simplemde";
 
+let simplemde;
 export default {
   data() {
     return {
-      
     };
   },
   mounted: function() {
     simplemde = new SimpleMDE({
-      element: document.getElementById("editor"),
-      placeholder: "Type here..."
+      autosave: {
+            enabled: true,
+            uniqueId: "MyUniqueID",
+            delay: 1000
+        },
+        status: ["autosave", "lines", "words"],
+        spellChecker: false,
+        element: document.getElementById("editor"),
+        promptURLs: true,
+        toolbar: [
+            "bold", "italic", "strikethrough", "heading", "code", "quote", "unordered-list",
+            "ordered-list", "clean-block", "link", "image", "table", "horizontal-rule", "preview", "side-by-side", "fullscreen", "guide",
+            {
+                name: "uploadImage",//自定义按钮
+                action: function customFunction(editor) {
+                    console.log(editor);
+                },
+                className: "fa fa-star",
+                title: "Upload Image"
+            }
+        ]
     });
   }
 };
 </script>
+
+<style lang="stylus" scoped>
+@import '..\..\assets\stylus\simplemde.styl'
+</style>
+
